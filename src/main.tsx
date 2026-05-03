9196e2754b2b6fc4f5ac8448d60131140a25ad2d
Narumikr <narunarumikumiku396@gmail.com>
2026-05-03 16:10:44 +0900

    untitled style

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@naru/untitled-ui-library/sekai-style.css';
 import './index.css';
 import { App } from './App';
 
