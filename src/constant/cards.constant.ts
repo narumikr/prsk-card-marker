d2e99542f23aae35781847e7cb8cb36603c957fb
Narumikr <narunarumikumiku396@gmail.com>
2026-06-25 00:14:24 +0900

    sekai colorが入力文字に適応されるように修正

--- src/constant/cards.constant.ts
+++ src/constant/cards.constant.ts
@@ -25,4 +25,22 @@ export const LookAtMyOshiCardText = {
  */
 export const OfficialProfileCardInputLabels = {
   name: '私の名前は',
+  birthdayMonth: '誕生月は',
+  birthdayDay: '誕生日は',
+  personality: '性格は',
+  specialSkill: '特技は',
+  actualy: '実は',
+  hobby: '趣味は',
+  food: '食べ物',
+  drink: '飲み物',
+  season: '季節',
+  animal: '動物',
+  place: '場所',
+  goods: 'グッズ',
+  cool: 'かっこいい人は',
+  kind: '優しい人は',
+  funny: '面白い人は',
+  swapBody: '一日入れ替われるとしたら誰',
+  withUninhabitedIsland: '無人島に連れて行きたい人は',
+  withKaraoke: 'カラオケに行きたい人は',
 };
