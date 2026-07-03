d1f706e18c71ecf5ca2773f1d0382de6ff9085a6
Naru <narunarumikumiku396@gmail.com>
2026-07-04 01:45:59 +0900

    公式プロフィールのカード追加 (#23)
    公式のプロフカードの追加

--- src/constant/cards.constant.ts
+++ src/constant/cards.constant.ts
@@ -1,15 +1,48 @@
 export const CARD_WIDTH = 960;
 export const CARD_HEIGHT = 540;
 
+export const OFFICIAL_CARD_WIDTH = 600;
+export const OFFICIAL_CARD_HEIGHT = 808;
+
 /**
  * @description BasicIntroductionCardコンポーネントのテキスト定義
  */
 export const BasicIntroductionCardText = {
   cardTitle: 'プロセカ自己紹介カード',
   inputLabels: ['Name', 'プロセカID', '推しキャラ', '推しユニット', '好きなところ', '好きな楽曲'],
+  freeSpace: 'フリースペース',
 };
 
+/**
+ * @description LookAtMyOshiCardコンポーネントのテキスト定義
+ */
 export const LookAtMyOshiCardText = {
   leftLabels: ['推しの名前', '誕生日', '好きな髪型', '好きな衣装', '推し x 3DMV'],
   rightLabels: ['好きなカード', '好きなストーリー', 'きっかけ', '推しポイント', 'もっと推しポイント'],
+};
+
+/**
+ * @description OfficialProfileCardコンポーネントのInput要素のラベル定義
+ */
+export const OfficialProfileCardInputLabels = {
+  name: '私の名前は',
+  birthdayMonth: '誕生月は',
+  birthdayDay: '誕生日は',
+  personality: '性格は',
+  specialSkill: '特技は',
+  actualy: '実は',
+  hobby: '趣味は',
+  food: '食べ物',
+  drink: '飲み物',
+  season: '季節',
+  animal: '動物',
+  place: '場所',
+  goods: 'グッズ',
+  cool: 'かっこいい人は',
+  kind: '優しい人は',
+  funny: '面白い人は',
+  swapBody: '一日入れ替われるとしたら誰',
+  withUninhabitedIsland: '無人島に連れて行きたい人は',
+  withKaraoke: 'カラオケに行きたい人は',
+  freeSpace: '自由記入欄',
 };
