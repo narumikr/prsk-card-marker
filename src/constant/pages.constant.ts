b8b480f5abbd0bd8aac593b747a4825a4c559aa9
Narumikr <narunarumikumiku396@gmail.com>
2026-07-05 04:43:48 +0900

    エクスポート機能用のUI文字列を追加

--- src/constant/pages.constant.ts
+++ src/constant/pages.constant.ts
@@ -5,5 +5,8 @@
 export const TOP_PAGE_TEXT = {
   profileFileName: 'profile.png',
   saveImageButtonLabel: '画像として保存',
+  exportingLabel: '出力中...',
+  exportSuccessLabel: '保存しました',
+  exportErrorMessage: '画像出力に失敗しました。もう一度お試しください',
   genImageErrorLog: '画像の生成に失敗しました',
 } as const;
